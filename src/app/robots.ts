33fcc54aa709c946408569139692f23d6d242405
Addisreview <worku.direct@gmail.com>
2026-05-21 21:03:24 -0400

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/auth/'],
     },
-    sitemap: 'https://addisreview.co/sitemap.xml',
+    sitemap: 'https://www.addisreviews.com/sitemap.xml',
   };
 }
